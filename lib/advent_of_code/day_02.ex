d60000b2fc3b4f2a764de291b1e1f79b44ae6187
Nate Todd <nate@natetodd.com>
2021-12-03 03:03:57 +0000

    Day 2 part 1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    args
+    |> String.splitter("\n", trim: true)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn [direction, count] -> [direction, String.to_integer(count)] end)
+    |> drive({0, 0})
   end
 
   def part2(args) do
   end
+
+  defp drive([step | tail], acc) do
+    {x, y} = acc
+
+    case step do
+      ["forward", count] ->
+        drive(tail, {x + count, y})
+
+      ["down", count] ->
+        drive(tail, {x, y + count})
+
+      ["up", count] ->
+        drive(tail, {x, y - count})
+    end
+  end
+
+  defp drive([], {x, y}), do: x * y
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,12 +3,26 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+      forward 2
+      down 4
+      down 1
+      down 4
+      forward 3
+      down 6
+      down 5
+      forward 3
+      forward 8
+      down 2
+      down 3
+      up 8
+      down 5
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 16 * 22
   end
 
   @tag :skip
